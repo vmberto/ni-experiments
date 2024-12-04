8bb7949c53d9de3df5464b93a601ae9731acb2e0
UmbertoB <umbertobarrosf@gmail.com>
2024-12-03 21:05:56 -0300

    new experiments config file

--- experiments_config_old.py
+++ experiments_config_old.py
@@ -1,5 +1,6 @@
 from layers.default_aug import get_default_aug_layers as DefaultAug
 from models.resnet import ResNet50Model
+from models.xception import XceptionModel
 from layers.salt_and_pepper import RandomSaltAndPepper
 import keras.layers as layers
 from keras_cv.core import UniformFactorSampler
@@ -75,6 +76,72 @@ CONFIGS = [
             ],
         ],
         "model": ResNet50Model,
+        "mixed": True,
+        "active": False,
+    },
+    {
+        "approach_name": 'Baseline',
+        "data_augmentation_layers": [],
+        "model": XceptionModel,
+        "mixed": False,
+        "active": False,
+    },
+    {
+        "approach_name": 'Salt&Pepper',
+        "data_augmentation_layers": [RandomSaltAndPepper(SALTPEPPER_FACTOR)],
+        "model": XceptionModel,
+        "mixed": False,
+        "active": False,
+    },
+    {
+        "approach_name": 'Gaussian',
+        "data_augmentation_layers": [layers.GaussianNoise(.1)],
+        "model": XceptionModel,
+        "mixed": False,
+        "active": False,
+    },
+    {
+        "approach_name": 'DefaultAug',
+        "data_augmentation_layers": DefaultAug(),
+        "model": XceptionModel,
+        "mixed": False,
+        "active": False,
+    },
+    {
+        "approach_name": 'DefaultAug+S&P',
+        "data_augmentation_layers": [
+            *DefaultAug(),
+            RandomSaltAndPepper(SALTPEPPER_FACTOR),
+        ],
+        "model": XceptionModel,
+        "mixed": False,
+        "active": False,
+    },
+    {
+        "approach_name": 'DefaultAug+Gaussian',
+        "data_augmentation_layers": [
+            *DefaultAug(),
+            layers.GaussianNoise(.1),
+        ],
+        "model": XceptionModel,
+        "mixed": False,
+        "active": False,
+    },
+    {
+        "approach_name": 'Mixed',
+        "data_augmentation_layers": [
+            None,
+            DefaultAug(),
+            [
+                *DefaultAug(),
+                layers.GaussianNoise(.1),
+            ],
+            [
+                *DefaultAug(),
+                RandomSaltAndPepper(SALTPEPPER_FACTOR),
+            ],
+        ],
+        "model": XceptionModel,
         "mixed": True,
         "active": False,
     },
